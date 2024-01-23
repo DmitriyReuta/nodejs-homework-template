7d9d9e6f2f6a971266728efeb92e570bf34a68cd
Dmitriy Reuta <reutadmitriy@gmail.com>
2024-01-23 20:07:46 +0200

    dassda

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -87,7 +87,7 @@ const login = async (req, res, next) => {
         }
 
         const user = await User.findOne({ email });
-        if (!user) {
+        if (!user.verify) {
             throw HttpError(401, "Email or password is wrong");
         }
 
